e4669cd0d0a882bd59ead0e565a949c376b87aab
Max Veit <maxdveit@gmail.com>
2013-05-29 23:20:46 -0500

    Tested for thread-safety, fixed bugs, confirmed everything works as expected. No copy-constructor yet.

--- devel2013/BufferThreaded1.cpp
+++ devel2013/BufferThreaded1.cpp
@@ -1,9 +1,11 @@
 #include <pthread.h>
 #include <boost/function.hpp>
 #include <boost/bind.hpp>
+#include <unistd.h>
 
 using boost::function;
 using boost::bind;
+using namespace std;
 
 /**
  * Wrapper for interfacing with C-style pthreads library (so C linkage may or
@@ -38,10 +40,10 @@ class BufferThreaded {
 
     int readFromSensor(int** dataIn) {
         // Dummy data-filling method.
-        int numItems = 4;
+        ++numItems;
         *dataIn = new int[numItems];
         for (int i = 0; i < numItems; i++) {
-            (*dataIn)[i] = 5; // For example...
+            (*dataIn)[i] = i;
         }
         return numItems;
     }
@@ -62,6 +64,7 @@ class BufferThreaded {
         // Stop the thread and take care of the extra pthreads destruction
         // requirements
         pthread_cancel(read_thread);
+        pthread_join(read_thread, NULL);
 
         pthread_cond_destroy(&read_cond);
         pthread_mutex_destroy(&data_mtx);
@@ -118,15 +121,22 @@ class BufferThreaded {
 
     /**
      * The updater thread function. This function is not meant to return; it
-     * simply runs until canceled.
+     * simply runs until the thread is canceled.
      *
      * It is called from an external wrapper function.
      */
     void* threadMeth() {
+        bool bUpl; // Thread-local updating flag
         while (true) {
             pthread_mutex_lock(&upfl_mtx);
             pthread_cond_wait(&read_cond, &upfl_mtx);
-            if (bUpdating) { // Is it OK to proceed?
+            /* Keep the critical section as short as possible (we don't want
+             * to block callers of isUpdating() ).
+             */
+            bool bUpl = bUpdating;
+            pthread_mutex_unlock(&upfl_mtx);
+
+            if (bUpl) { // Is it OK to proceed?
                 /* This is the actual bulk of the update functionality.
                  * It can (and should) be delegated to separate functions.
                  */
@@ -136,9 +146,10 @@ class BufferThreaded {
                  */
                 sleep(5);
 
-                /* Keep the section in between the lock guards as short and
-                 * fast as possible. It should consist only of copying the data
-                 * received from the sensor into the internal buffer variables.
+                /* Keep the section in between the lock guards (i.e. the
+                 * "critical section") as short and fast as possible. It should
+                 * consist only of copying the data received from the sensor
+                 * into the internal buffer variables.
                  *
                  * The reason is that other threads (like the main thread) may
                  * want to access data using the get-functions while this
@@ -150,8 +161,12 @@ class BufferThreaded {
                 delete[] data;
                 numItems = readFromSensor(&data);
                 pthread_mutex_unlock(&data_mtx);
+
+                // Report that we are done updating.
+                pthread_mutex_lock(&upfl_mtx);
+                bUpdating = false;
+                pthread_mutex_unlock(&upfl_mtx);
             }
-            pthread_mutex_unlock(&upfl_mtx);
         }
     }
 };
@@ -162,5 +177,64 @@ void* pthreadWrapper(void* arg) {
     // Aaugh! My eyes!
     function<void* ()>* tFun = static_cast<function<void* ()>* >(arg);
     return (*tFun)();
+}
+
+/**
+ * Interactive testing of the functionality of the threaded buffer.
+ */
+int main(int argc, char** argv) {
+    bool bContinue = true;
+    bool bReset = false;
+    char cmd;
+    int *data;
+    int numItems;
+    BufferThreaded* buf;
+    buf = new BufferThreaded();
+    buf->spawnThreads();
+    while (bContinue) {
+        if (bReset) {
+            bReset = false;
+            delete(buf); // This allows testing of clean and safe deletion
+            buf = new BufferThreaded();
+            buf->spawnThreads();
+        }
+        cout << "Please enter a command out of " <<
+            "{\'u\', \'g\', \'i\', \'r\', \'q\'}: ";
+        cin >> cmd;
+        cout << endl;
+        switch (cmd) {
+        case 'u':
+            // Update.
+            buf->readData();
+            break;
+        case 'g':
+            // Get data.
+            numItems = buf->getData(&data);
+            cout << "Got data: [";
+            for (int i = 0; i < numItems; i++) {
+                cout << data[i] << ", ";
+            }
+            cout << "]" << endl;
+            delete(data);
+            break;
+        case 'i':
+            // Inquiry, isUpdating.
+            cout << "Updating: " << buf->isUpdating() << endl;
+            break;
+        case 'r':
+            // Reset state.
+            cout << "Resetting buffer." << endl;
+            bReset = true;
+            break;
+        case 'q':
+            // Quit.
+            bContinue = false;
+            break;
+        default:
+            cout << "Unknown command. Exiting." << endl;
+            return -1;
+        }
+    }
+    return 0;
 }
 
